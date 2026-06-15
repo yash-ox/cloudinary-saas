5a58bee6cb7d5302a6e3e8258665324bfed90f0d
Yash <ys28528@gmail.com>
2026-06-15 23:04:05 +0530

    chore: add axios dependency to package.json

--- app/(app)/video-upload/page.tsx
+++ app/(app)/video-upload/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 function VideoUpload() {
   return <div>VideoUpload</div>;
